6e1347a66a0a517ed306500e4a3cf53818530a95
Mayyyyyra <narvatkinavv@gmail.com>
2025-08-14 16:58:06 +0300

    Add function

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
 import pandas as pd
 from statsmodels.stats.proportion import proportions_ztest
 
-# Нулевая гипотиза H0: Количество событий в группах control и test одинаково
+# Нулевая гипотиза H0: Количество событий в группах control и test не изменились
 data = pd.read_csv('./data/user_event_data_streaming_mini.csv')
+data_unique = data['user_id'].unique()
+print(data_unique)
 
 # Подсчет количества событий для каждой группы
 control_events = data[data['group'] == 'control']['event'].value_counts()
 test_events = data[data['group'] == 'test']['event'].value_counts()
 
-print(control_events, test_events)
-
-# Количество событий в каждой группе
-control_count = control_events['add_to_playlist']
-test_count = test_events['add_to_playlist']
-
 # Общее количество пользователей в каждой группе
-total_control = data[data['group'] == 'control'].shape[0]
-total_test = data[data['group'] == 'test'].shape[0]
+total_control = data[data['group'] == 'control']
+total_test = data[data['group'] == 'test']
 
-# Проведение z-теста
-stat, p_value = proportions_ztest([control_count, test_count], [total_control, total_test])
+def zero_hypothesis(event: str):
+    control_count = control_events[event]
+    test_count = test_events[event]
 
-print(f"Z-статистика: {stat}, p-значение: {p_value}")
+    stat, p_value = proportions_ztest([control_count, test_count], [total_control, total_test])
 
-alpha = 0.05
-if p_value < alpha:
-    print("Отвергаем нулевую гипотезу, группы различаются.")
-else:
-    print("Не отвергаем нулевую гипотезу, различий нет.")
+    print(f"Для {event}: Z-статистика: {stat}, p-значение: {p_value}")
+
+    alpha = 0.05
+    if p_value < alpha:
+        print("Отвергаем нулевую гипотезу, группы различаются. \n")
+    else:
+        print("Не отвергаем нулевую гипотезу, различий нет.\n")
+
+unique_events = data['event'].unique()
+
+for event in unique_events:
+    zero_hypothesis(event)
